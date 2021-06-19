c09cdc73165ae47847f82614ad3e3c83e522f0f6
Viktor Bem <malchikk@live.com>
2021-06-19 14:48:56 +0200

    add logic to plus-minus and percentage buttons

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const lcd = document.getElementById("display");
 const numbers = document.querySelectorAll("#numbers button");
 const operators = document.querySelectorAll("#operators button");
 const operation = document.getElementById("operation");
+const percIndicator = document.getElementById("percIndicator");
 
 var cache = [];
 var result = 0;
@@ -15,9 +16,11 @@ for (var operator of operators) {
 }
 
 document.getElementById("ac").addEventListener("click", clearDisplay);
-
+document.getElementById("plusmin").addEventListener("click", plusMinus);
+document.getElementById("perc").addEventListener("click", percentage);
 
 function appendNumber(number) {
+    percIndicator.innerHTML = "";
     if (number.name === "coma") {
         if (!(cache.includes("."))) {
             cache.push(".");
@@ -29,6 +32,7 @@ function appendNumber(number) {
 }
 
 function queueOperation(operator) {
+    percIndicator.innerHTML = "";
     if (operator.name === "result") {
         calculate();
     } else {
@@ -42,6 +46,38 @@ function queueOperation(operator) {
         cache = [];
         operation.innerHTML = operator.name;
     }
+}
+
+function plusMinus() {
+    var display = lcd.innerHTML.toString().split("");
+    if (display[0] == "-") {
+        display.shift();
+    } else {
+        display.unshift("-");
+    }
+    lcd.innerHTML = display.join("");
+}
+
+function percentage() {
+    var number = parseFloat(lcd.innerHTML);
+    if (operation.innerHTML === "add") {
+        result = result + (result / 100 * number);
+    } else if (operation.innerHTML === "subtract") {
+        result = result - (result / 100 * number);
+    } else if (operation.innerHTML === "multiply") {
+        result = result / 100 * number;
+    } else if (operation.innerHTML === "divide") {
+        if (number === 0) {
+            result = 0;
+        } else {
+            result = (result / number) * 100;
+        }
+    } else {
+        result = number;
+    }
+    lcd.innerHTML = result;
+    percIndicator.innerHTML = "perc";
+    clearAll();
 }
 
 function calculate() {
@@ -72,6 +108,7 @@ function clearDisplay() {
     }
     cache = [];
     lcd.innerHTML = "0";
+    percIndicator.innerHTML = "";
 }
 
 function clearAll() {
